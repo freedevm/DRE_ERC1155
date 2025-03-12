24912db71e10fa2e0de589654ce488a6a42d7d51
freedevm <freedevmdan@gmail.com>
2025-03-12 08:53:31 +0800

    updated some features

--- ignition/modules/TestMint.js
+++ ignition/modules/TestMint.js
@@ -1,19 +1,19 @@
 const hre = require("hardhat");
 
-const contractAddress = '0x5711995e40ad8eba9748fd546cd399bbb6baf15c';
+const contractAddress = '0x7C2a827254B7a6b8dE57F1547409c8677188A1dd';
 const ownerAddress = '0xEA38026AF023cc689659a9D2A35046f8980821f2';
 
 async function mint() {
-    const contract = await hre.ethers.getContractAt("DRE", contractAddress);
+    const dre = await hre.ethers.getContractAt("DRE", contractAddress);
 
-    // await contract.mint(ownerAddress, amount, collection ID);
+    // await dre.mint(ownerAddress, amount, collection ID);
     // amount = 1
 
-    await contract.mint(ownerAddress, 1, 0); // Mint 1 Music NFT
-    await contract.mint(ownerAddress, 1, 1); // Mint 1 Artwork NFT
+    await dre.mint(ownerAddress, 1, 0); // Mint 1 Music NFT
+    // await dre.mint(ownerAddress, 1, 1); // Mint 1 Artwork NFT
 
     console.log("Minted 1 Music NFT:", await dre.balanceOf(ownerAddress, 0))
-    console.log("Minted 1 Artwork NFT:", await dre.balanceOf(ownerAddress, 1))
+    // console.log("Minted 1 Artwork NFT:", await dre.balanceOf(ownerAddress, 1))
 
     
 }
